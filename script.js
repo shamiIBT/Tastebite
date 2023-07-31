c820afc0e7c9b5569dbcf4ac58fbff2b59468245
shamiIBT <122720034+shamiIBT@users.noreply.github.com>
2023-07-31 15:24:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+<div class="section--2-card">
+<div class="card-img-div">
+  <img src="/resources/menu3.jpg" alt="" class="card-img" />
+</div>
+<div class="card-descp">
+  <div class="card-descp-stars">
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+  </div>
+  <a href="" class="card-link">Fancy Glazed Donughts</a>
+</div>
+</div>
+<div class="section--2-card">
+<div class="card-img-div">
+  <img src="/resources/menu4.jpg" alt="" class="card-img" />
+</div>
+<div class="card-descp">
+  <div class="card-descp-stars">
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+    <span class="fa fa-star checked"></span>
+  </div>
+  <a href="" class="card-link">Mighty Cheesy Breakfast Burger</a>
+</div>
+</div>
